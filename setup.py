adf9a34915d428274c46e445dd4eb292ef987f1f
sjLee <lsjj096@gmail.com>
2020-03-12 00:37:20 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import setuptools
+
+_VERSION = '0.1'
+
+REQUIRED_PACKAGES = [
+]
+
+DEPENDENCY_LINKS = [
+]
+
+setuptools.setup(
+    name='DepthwiseSeparableConvolution_Pytorch',
+    version=_VERSION,
+    description="Unofficial PyTorch modification of Depthwise Separable Convolution",
+    install_requires=REQUIRED_PACKAGES,
+    dependency_links=DEPENDENCY_LINKS,
+    url='https://github.com/seungjunlee96/DepthwiseSeparableConvolution_Pytorch',
+    license='MIT License',
+    include_package_data=True,
+    package_dir={},
+    packages=setuptools.find_packages(exclude=['tests']),
+)
