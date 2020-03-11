e3b16ac3cf40b8d26b7278003450ed808f7239c9
sjLee <lsjj096@gmail.com>
2020-03-12 01:31:33 +0900

    Update DepthwiseSeparableConvolution.py

--- DepthwiseSeparableConvolution/DepthwiseSeparableConvolution.py
+++ DepthwiseSeparableConvolution/DepthwiseSeparableConvolution.py
@@ -1,3 +1,5 @@
+import torch.nn as nn
+
 class depthwise_separable_conv(nn.Module):
     def __init__(self, nin, nout, kernel_size = 3, padding = 1, bias=False):
         super(depthwise_separable_conv, self).__init__()
